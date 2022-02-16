0d217b857d2c31d818fe2b60b7f5f80acf00c5e2
ProfMara <94469709+ProfMara@users.noreply.github.com>
2022-02-16 13:44:05 -0300

    Add files via upload

---
+++ ATIVIDADE 1/sketch.js
@@ -0,0 +1,27 @@
+var notas = [50, 80, 74, 60, 65, 98, 99, 94, 52, 13, 75, 64, 2, 3, 10];
+
+var quantidade = notas.;
+
+
+function setup() {
+    createCanvas(800, 800);
+    console.log(notas);
+    notas.push(78);
+    console.log(notas);
+    console.log(quantidade);
+
+
+    for (var i = 0; i < ; i++) {
+        console.log()
+    }
+
+}
+
+function draw() {
+
+    background("black");
+
+
+    drawSprites();
+
+}
